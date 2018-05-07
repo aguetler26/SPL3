e4896d44f15dad988514cc91c4fa6bf525fc95c8
aguetler26 <aguetler26@icapital.at>
2018-05-07 13:51:51 +0200

    FizzBuzz, Version 1

---
+++ src/FizzBuzz.java
@@ -0,0 +1,21 @@
+public class FizzBuzz {
+	public static void main(String[] args) {
+		for (int n = 1; n <= 100; n++) {
+			FizzBuzz(n);
+		}
+		System.out.println("ende!");
+	}
+	
+	public static void FizzBuzz(int n) {
+		if (n % 3 == 0 && n % 5 == 0) {
+			System.out.print("FizzBuzz ");
+		} else if (n % 3 == 0) {
+			System.out.print("Fizz ");
+		} else if (n % 5 == 0) {
+			System.out.print("Buzz ");
+		} else {
+			System.out.print(n);
+		}
+		System.out.println();
+	}
+}
