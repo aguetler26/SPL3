94a9e9aeec8ca7b77b779d59e8131a0953ef1792
aguetler26 <aguetler26@icapital.at>
2018-05-07 14:11:54 +0200

    FizzerBuzzer Version 2

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,18 +1,17 @@
 public class FizzBuzz {
 	public static void main(String[] args) {
 		for (int n = 1; n <= 100; n++) {
-			FizzBuzz(n);
+			FizzerBuzzer(n); // bei 4: fizzer, bei 7: buzzer
 		}
-		System.out.println("ende!");
 	}
 	
-	public static void FizzBuzz(int n) {
-		if (n % 3 == 0 && n % 5 == 0) {
-			System.out.print("FizzBuzz ");
-		} else if (n % 3 == 0) {
-			System.out.print("Fizz ");
-		} else if (n % 5 == 0) {
-			System.out.print("Buzz ");
+	public static void FizzerBuzzer(int n) {
+		if (n % 4 == 0 && n % 7 == 0) {
+			System.out.print("FizzerBuzzer ");
+		} else if (n % 4 == 0) {
+			System.out.print("Fizzer ");
+		} else if (n % 7 == 0) {
+			System.out.print("Buzzer ");
 		} else {
 			System.out.print(n);
 		}
